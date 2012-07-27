c48ef0d1c54bcb5cf3d8105c03d932cfd40e25d9
mario <mario16@poczta.fm>
2012-07-27 19:20:09 +0200

    Added class struct for Users and tested it

--- index.php
+++ index.php
@@ -4,38 +4,19 @@ include 'lib/bones.php';
 
 get('/signup', function($app) {
 	// echo curl_version();
-	var_dump(function_exists("curl_init"));
+	// var_dump(function_exists("curl_init"));
 	$app->render('signup');
 	
 });
 
 post('/signup', function($app) {
 	// echo 'mario';
-	$user = new stdClass();
-	$user->type = 'user';
+	$user = new User();
 	$user->name = $app->form('name');
 	$user->email = $app->form('email');
 
-	echo json_encode($user);
-
-	$curl = curl_init();
-
-	$options = array(
-		CURLOPT_URL => 'localhost:5984/verge',
-		CURLOPT_POSTFIELDS => json_encode($user),
-		CURLOPT_HTTPHEADER => array("Content-Type: application/json"),
-		CURLOPT_CUSTOMREQUEST => 'POST',
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "utf-8",
-		CURLOPT_HEADER => false,
-		CURLOPT_FOLLOWLOCATION => true,
-		CURLOPT_AUTOREFERER => true
-	);
-
-	curl_setopt_array($curl, $options);
-
-	curl_exec($curl);
-	curl_close($curl);
+	// echo var_dump($user->to_json());
+	$app->couch->post($user->to_json());
 
 	$app->set('message', 'Thanks for Signing Up ' . $app->form('name') . '!');
 	$app->render('home');
